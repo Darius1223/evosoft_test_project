cbf7c887476203b1381bd71e44036402557c841d
Darius1223 <dar.290199@mail.ru>
2022-11-12 12:18:55 +0700

    EDIT: add Note, User models in admin panel

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -6,3 +6,13 @@ from apps.core.models import Diary, User, Note
 @admin.register(Diary)
 class DiaryAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
